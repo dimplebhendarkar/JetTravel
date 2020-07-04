f789f9744dbe3383d8e0a7c14bfd6097d402d43b
Dimpal Bhendarkar <dimpal.bhendarkar@gmail.com>
2020-07-04 14:09:08 +0530

    Initial Commit

---
+++ JetTravel/JetTravel/Classes/Feed/JTFeedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  JTFeedViewController.swift
+//  JetTravel
+//
+//  Created by Nishant Kumbhare on 04/07/20.
+//  Copyright © 2020 DimpalBhendarkar. All rights reserved.
+//
+
+import UIKit
+
+class JTFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+}
